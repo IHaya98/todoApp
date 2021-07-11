59eac4be3907c2deac995fd8e58e53e84d992cfe
IHaya98 <sakura98eight@gmail.com>
2021-07-11 18:44:43 +0900

    loginデザイン修正

--- lib/ui/signin/signin_screen.dart
+++ lib/ui/signin/signin_screen.dart
@@ -23,49 +23,118 @@ class SigninScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('ログイン'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) => value!.isEmpty ? 'Eメールは必須項目' : null,
-            onChanged: (value) => context.read(signinProvider).email = value,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Password'),
-            validator: (value) => value!.isEmpty ? 'パスワードは必須項目' : null,
-            onChanged: (value) => context.read(signinProvider).password = value,
-            obscureText: true,
-          ),
+      body: Stack(
+        children: [
           Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              child: const Text('ログイン'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.orange,
-                onPrimary: Colors.white,
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/image/login.png'),
+                fit: BoxFit.cover,
               ),
-              onPressed: () {
-                FBAuth().signIn(context.read(signinProvider).email,
-                    context.read(signinProvider).password, context);
-              },
             ),
           ),
           Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              child: const Text('新規登録'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.orange,
-                onPrimary: Colors.white,
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+                  const Color(0xffff5500).withOpacity(0.2),
+                  const Color(0xff00bbaa).withOpacity(0.8),
+                ],
+                stops: const [
+                  0.0,
+                  1.0,
+                ],
               ),
-              onPressed: () {
-                Navigator.of(context).push(
-                  SignupScreen.route(),
-                );
-              },
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'xxx@xxx.xxx',
+                      icon: Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'Eメールは必須項目' : null,
+                    onChanged: (value) =>
+                        context.read(signinProvider).email = value,
+                  ),
+                ),
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(
+                        Icons.password,
+                        color: Colors.white,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'パスワードは必須項目' : null,
+                    onChanged: (value) =>
+                        context.read(signinProvider).password = value,
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 350,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text('ログイン'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.white,
+                        onPrimary: Colors.black,
+                      ),
+                      onPressed: () {
+                        FBAuth().signIn(context.read(signinProvider).email,
+                            context.read(signinProvider).password, context);
+                      },
+                    ),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 350,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: Text('新規登録'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          SignupScreen.route(),
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
--- lib/ui/signup/signup_screen.dart
+++ lib/ui/signup/signup_screen.dart
@@ -22,42 +22,118 @@ class SignupScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text('新規登録'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Email'),
-            validator: (value) => value!.isEmpty ? 'Eメールは必須項目' : null,
-            onChanged: (value) => context.read(signupProvider).email = value,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Username'),
-            validator: (value) => value!.isEmpty ? 'ユーザー名は必須項目' : null,
-            onChanged: (value) =>
-                context.read(signupProvider).user_name = value,
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: 'Password'),
-            validator: (value) => value!.isEmpty ? 'パスワードは必須項目' : null,
-            onChanged: (value) => context.read(signupProvider).password = value,
-            obscureText: true,
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/image/login.png'),
+                fit: BoxFit.cover,
+              ),
+            ),
           ),
           Container(
-            padding: const EdgeInsets.all(16),
-            alignment: Alignment.center,
-            child: ElevatedButton(
-              child: const Text('登録'),
-              style: ElevatedButton.styleFrom(
-                primary: Colors.orange,
-                onPrimary: Colors.white,
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: FractionalOffset.topCenter,
+                end: FractionalOffset.bottomCenter,
+                colors: [
+                  const Color(0xffff5500).withOpacity(0.2),
+                  const Color(0xff00bbaa).withOpacity(0.8),
+                ],
+                stops: const [
+                  0.0,
+                  1.0,
+                ],
               ),
-              onPressed: () {
-                FBAuth().signUp(
-                    context.read(signupProvider).email,
-                    context.read(signupProvider).user_name,
-                    context.read(signupProvider).password,
-                    context);
-              },
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Email',
+                      hintText: 'xxx@xxx.xxx',
+                      icon: Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'Eメールは必須項目' : null,
+                    onChanged: (value) =>
+                        context.read(signupProvider).email = value,
+                  ),
+                ),
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Username',
+                      icon: Icon(
+                        Icons.mail,
+                        color: Colors.white,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'ユーザー名は必須項目' : null,
+                    onChanged: (value) =>
+                        context.read(signupProvider).user_name = value,
+                  ),
+                ),
+                Container(
+                  width: 350,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      icon: Icon(
+                        Icons.password,
+                        color: Colors.white,
+                      ),
+                      hintStyle: TextStyle(color: Colors.white),
+                      labelStyle: TextStyle(color: Colors.white),
+                    ),
+                    validator: (value) => value!.isEmpty ? 'パスワードは必須項目' : null,
+                    onChanged: (value) =>
+                        context.read(signupProvider).password = value,
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  alignment: Alignment.center,
+                  child: SizedBox(
+                    width: 350,
+                    height: 50,
+                    child: ElevatedButton(
+                      child: const Text('登録'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Colors.orange,
+                        onPrimary: Colors.white,
+                      ),
+                      onPressed: () {
+                        FBAuth().signUp(
+                            context.read(signupProvider).email,
+                            context.read(signupProvider).user_name,
+                            context.read(signupProvider).password,
+                            context);
+                      },
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
